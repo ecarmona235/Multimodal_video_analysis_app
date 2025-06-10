e42f645f7a2e6ef2d10a3ab7abede7b68922bd44
ecarmona235 <ecarmona235@gmail.com>
2025-06-09 22:38:03 -0700

    Initializing repo

--- nextjs-folder/src/utils/redis.js
+++
@@ -1,7 +0,0 @@
-import Redis from "ioredis";
-
-// This file sets up a Redis client using ioredis.// It connects to a Redis server using the URL specified in the environment variable REDIS_URL.
-// If the environment variable is not set, it defaults to 'redis://localhost:6379'.
-const redis = new Redis(process.env.REDIS_URL || "redis://localhost:6379");
-
-export default redis;
